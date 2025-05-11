2eef9bfade673c57dcd10ff165aede7af98ff762
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-05-12 02:34:03 +0300

    Create the StartupTestDrive class
    This class contains test code that we will use to test the fuctionality
    of the Startup class that we will create in the next commit.
    
    create a fake startup location and fake user guess and then
    test checkYourself method on them.

---
+++ StartupTestDrive.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class StartupTestDrive {
+  
+  public static void main(String[] args) {
+    
+    Startup testStartup = new Startup();
+    ArrayList<String> fakeLocation = new ArrayList<String>();
+    fakeLocation.add("A0");
+    fakeLocation.add("A1");
+    fakeLocation.add("A3");
+    
+    String fakeUserGuess = "A1";
+    String result = testStartup.checkYourself(fakeUserGuess);
+
+    if(result.equals("hit")) {
+      System.out.println("passed");
+    }else { 
+      System.out.println("failed");
+    }
+
+  }
+}
